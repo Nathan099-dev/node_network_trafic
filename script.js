90732642f7aebffb33049c2fa47ce737c2de180d
Nathan099-dev <tonininathan8@gmail.com>
2024-11-27 14:17:50 -0300

    finalizando programa

--- script.js
+++ script.js
@@ -1,13 +1,35 @@
-const dgram =  require('dgram');
+const dgram = require('dgram');
 const net = require('net')
 
 const updServer = dgram.createSocket('udp4');
-updServer.on('message',(msg, rinfo) => {
+updServer.on('message', (msg, rinfo) => {
     console.log(`Pacote udp de ${rinfo.address} : ${rinfo.port}`)
 });
 
-updServer.on('listening', () =>{
+updServer.on('listening', () => {
     const address = updServer.address()
     console.log(`Servidor UDP executando em ${address.address} :${address.port}`)
-    
 })
+
+updServer.bind(12345);
+
+const tcpServer = net.createSocket((socket) => {
+    console.log(`Conexão tcp de ${socket.remoteAdress} : ${socket.remotePort}`)
+
+    socket.on('data', (data) => {
+        console.log(`Dados recebidos de: ${data}`)
+    })
+
+    socket.on('close', () => {
+        console.log(`Conexão tcp `)
+    })
+})
+
+tcpServer.listen(12346, () => {
+    console.log('Servidor TCP escutando na porta 12346');
+  });
+
+
+
+
+c
