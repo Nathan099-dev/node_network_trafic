e9ee0bd10a23fd5a7725a7d4151e49e547c25dc8
Nathan Tonini <tonininathan8@gmail.com>
2024-11-28 14:03:10 +0000

    removendo ponto e vírgula

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const net = require('net')
 const updServer = dgram.createSocket('udp4');
 updServer.on('message', (msg, rinfo) => {
     console.log(`Pacote udp de ${rinfo.address} : ${rinfo.port}`)
-});
+})
 
 updServer.on('listening', () => {
     const address = updServer.address()
